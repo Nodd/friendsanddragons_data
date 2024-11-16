dcd0ec1b5477d8c3f3050f71d8bcf85f4e2375b5
Joseph Martinot-Lagarde <contrebasse@gmail.com>
2024-11-16 16:43:09 +0100

    More db

--- db.py
+++ db.py
@@ -5,12 +5,13 @@ from pprint import pprint
 class Hero:
     def __init__(self, data):
         self._data = data
+        self.known = False
 
     def __getattr__(self, name):
         return self._data[name]
 
     @property
-    def traits(self):
+    def all_traits(self):
         traits = list(self._data["basic_traits"])
         traits.extend(
             (
@@ -21,23 +22,84 @@ class Hero:
         )
         return traits
 
+    @property
+    def all_gear(self):
+        gear = []
+        for asc in ["basic_gear", "asc_gear", "asc2_gear", "asc3_gear"]:
+            gear.extend(self._data[asc].values())
+        return gear
+
+
+class Gear:
+    GEAR_RANK = ["Magic", "Epic", "Mythic", "Legendary", "Exalted", "Divine"]
+    AMULET_RANK = ["Radiant", "Brilliant", "Coruscating", "Incandescent"]
+    GEAR_KIND = ["amulet", "weapon", "ring", "head", "off_hand", "body"]
+
+    def __init__(self, name, kind):
+        rank, name = name.split(" ", 1)
+        self.rank = rank
+        self.name = name
+        self.kind = kind
+
+    @property
+    def rank_num(self):
+        if self.kind == "amulet":
+            return self.AMULET_RANK.index(self.rank)
+        else:
+            return self.GEAR_RANK.index(self.rank)
+
+    @property
+    def next_ranks(self):
+        if self.kind == "amulet":
+            ranks = self.AMULET_RANK[self.rank_num + 1 :]
+        else:
+            ranks = self.GEAR_RANK[self.rank_num + 1 :]
+        return [f"{rank} {self.name}" for rank in ranks]
+
+    @property
+    def previous_ranks(self):
+        if self.kind == "amulet":
+            ranks = self.AMULET_RANK[: self.rank_num]
+        else:
+            ranks = self.GEAR_RANK[: self.rank_num]
+        return [f"{rank} {self.name}" for rank in ranks]
+
 
 class HeroesDB:
     def __init__(self, heroes):
-        self._heroes = [Hero(data) for data in heroes]
+        self._heroes = {data["name"]: Hero(data) for data in heroes}
 
     @property
-    def traits(self):
+    def heroes(self):
+        yield from self._heroes.values()
+
+    @property
+    def all_traits(self):
         traits = set()
-        for hero in self._heroes:
-            for trait in hero.traits:
-                traits.add(trait)
+        for hero in self.heroes:
+            traits.update(hero.all_traits)
         return traits
 
+    @property
+    def all_gear(self):
+        all_gear = set()
+        for hero in self.heroes:
+            all_gear.update(hero.all_gear)
+        return all_gear
 
-heroes = json.load(open("heroes.json"))
+    def by_stars(self, *stars: int):
+        return [hero for hero in self.heroes if hero.stars in stars]
+
+    def by_names(self, names: str | list[str]):
+        if isinstance(names, str):
+            names = self._heroes[names]
+        else:
+            return [hero for hero in self._heroes if hero.name in names]
+
+
+with open("heroes.json") as f:
+    heroes = json.load(f)
 db = HeroesDB(heroes)
 
-
-print(len(db.traits))
-pprint(db.traits)
+pprint(db.all_traits)
+pprint(db.all_gear)
